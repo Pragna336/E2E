345e29ecc0ce1b6b58e266a8ddf6f225b09667f9
Pragna336 <pragnapapasani03@gmail.com>
2026-02-17 22:24:30 +0530

    Update script.js

--- Source/script.js
+++ Source/script.js
@@ -1,1 +1,12 @@
+function goHome(){
+    window.location.href = "home_page.html";
+}
+
+function showTab(id){
+    document.querySelectorAll(".tab").forEach(t=>t.classList.remove("active"));
+    document.querySelectorAll(".tab-content").forEach(c=>c.classList.remove("active"));
+
+    event.target.classList.add("active");
+    document.getElementById(id).classList.add("active");
+}
 
